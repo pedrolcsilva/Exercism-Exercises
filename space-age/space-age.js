12e9d06dfcd9c54c166e569debf77a8d04350c8e
pedrolcsilva <pedrolucas443@gmail.com>
2022-05-12 11:31:26 -0300

    Use Object for planets Changing the constants into an object to make it easier to return the result of the function

--- space-age/space-age.js
+++ space-age/space-age.js
@@ -2,37 +2,17 @@
 // This is only a SKELETON file for the 'Space Age' exercise. It's been provided as a
 // convenience to get you started writing code faster.
 //
-const MERCURY = 0.2408467;
-const VENUS = 0.61519726;
-const MARS = 1.8808158;
-const JUPITER = 11.862615;
-const SATURN = 29.447498;
-const URANUS = 84.016846;
-const NEPTUNE = 164.79132;
+const PLANETS = {
+  earth: 1,
+  mercury: 0.2408467,
+  venus: 0.61519726,
+  mars: 1.8808158,
+  jupiter: 11.862615,
+  saturn: 29.447498,
+  uranus: 84.016846,
+  neptune: 164.79132
+}
 export const age = (planet, earth_sec) => {
-  let age = earth_sec / (3600 * 24 * 365.25);
-  switch(planet){
-    case 'mercury':
-      age/= MERCURY;
-      break;
-    case 'venus':
-      age/= VENUS;
-      break;
-    case 'mars':
-      age/= MARS;
-      break;
-    case 'jupiter':
-      age/= JUPITER;
-      break;
-    case 'saturn':
-      age/= SATURN;
-      break;
-    case 'uranus':
-      age/= URANUS;
-      break;
-    case 'neptune':
-      age/= NEPTUNE;
-      break;
-  }
+  const age = earth_sec / (3600 * 24 * 365.25) / PLANETS[planet];
   return Math.round(age * 100)/ 100
 };
