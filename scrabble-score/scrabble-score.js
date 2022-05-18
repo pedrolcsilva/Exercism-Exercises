64c5a96c49a2c4c5cec91c2aee695b7654885ef6
pedrolcsilva <pedrolucas443@gmail.com>
2022-05-18 19:46:20 -0300

    Add Scrabble Score Exercise

---
+++ scrabble-score/scrabble-score.js
@@ -0,0 +1,40 @@
+//
+// This is only a SKELETON file for the 'Scrabble Score' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+const LETTER_VALUES = {
+  "A": 1, 
+  "E": 1, 
+  "I": 1,
+  "O": 1, 
+  "U": 1, 
+  "L": 1, 
+  "N": 1, 
+  "R": 1, 
+  "S": 1, 
+  "T": 1,
+  "D": 2,
+  "G": 2,
+  "B": 3, 
+  "C": 3, 
+  "M": 3, 
+  "P": 3,
+  "F": 4, 
+  "H": 4, 
+  "V": 4, 
+  "W": 4, 
+  "Y": 4,
+  "K": 5,
+  "J": 8,
+  "X": 8,
+  "Q": 10,
+  "Z": 10 
+}
+export const score = (word) => {
+  let points = 0;
+  let upperWord = word.toUpperCase().split('')
+  upperWord.forEach(letter =>{
+    points+= LETTER_VALUES[letter]
+  })
+  return points
+};
